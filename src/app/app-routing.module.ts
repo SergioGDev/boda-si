21a0fa39bb6b7ccaed331b95d6a5d6904938a602
SergioGDev <admin@sergiogdev.com>
2022-09-24 20:17:29 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ const routes: Routes = [
     loadChildren: () => import('./dashboard/dashboard.module').then( m => m.DashboardModule )
   }, {
     path: '**',
-    redirectTo: 'dashboard'
+    redirectTo: 'dashboard/ubicaciones'
   }
 ];
 
